6d5d9a7c24e797241cf7d6a9bb7f4c081b97710e
braianb92 <47337939+braianb92@users.noreply.github.com>
2019-02-05 16:06:21 -0300

    Add files via upload
    tarea: hecho al 7 inclusive (5/2)

--- 1-EntradaSalida/jsEntradaSalida-7.js
+++ 1-EntradaSalida/jsEntradaSalida-7.js
@@ -4,21 +4,62 @@ mostar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	
+    var num1;
+    var num2;
+    var resultado;
+
+    num1 = document.getElementById("numeroUno").value;
+
+    num2 = document.getElementById("numeroDos").value;
+
+    resultado = parseInt(num1) + parseInt(num2);
+
+    alert ("la suma es " + resultado);
 }
 
 function restar()
 {
-	
+    var num1;
+    var num2;
+    var resultado;
+
+    num1 = document.getElementById("numeroUno").value;
+
+    num2 = document.getElementById("numeroDos").value;
+
+    resultado = parseInt(num1) - parseInt(num2);
+
+    alert ("La resta es " + resultado);
 }
 
 function multiplicar()
 { 
-	
+   var num1;
+   var num2;
+   var resultado;
+   
+   num1 = document.getElementById("numeroUno").value;
+
+   num2 = document.getElementById("numeroDos").value;
+
+   resultado = parseInt(num1) * parseInt(num2);
+
+   alert ("La multiplicacion es " + resultado);
 }
 
 function dividir()
 {
-	
+    var num1;
+    var num2;
+    var resultado;
+    
+
+    num1 = document.getElementById("numeroUno").value;
+
+    num2 = document.getElementById("numeroDos").value;
+    
+    resultado = parseInt(num1) / parseInt(num2);
+
+    alert ("La division es " + resultado);
 }
 
