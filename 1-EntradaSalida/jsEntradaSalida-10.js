34cea1561155d03da5d3213d91bac2f4a3f90047
braianb92 <47337939+braianb92@users.noreply.github.com>
2019-02-06 01:40:16 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,16 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function MostrarDescuento()
 {
+    var importe;
+    var parcial;
+    var resultado;
+
+    importe = parseInt(document.getElementById("importe").value);
+
+    parcial = (importe * 25) / 100;
+
+    resultado = importe - parcial;
+
+    document.getElementById("resultado").value = resultado;
 	
 }
--- 1-EntradaSalida/jsEntradaSalida-9.js
+++ 1-EntradaSalida/jsEntradaSalida-9.js
@@ -4,6 +4,15 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function MostrarAumento()
 {
+    var sueldo;
+    var parcial;
+    var resultado;
 
-	
+    sueldo = parseInt(document.getElementById("sueldo").value);
+
+    parcial = (sueldo * 10) / 100;
+
+    resultado = sueldo + parcial;
+
+    document.getElementById("resultado").value = resultado;
 }
