af67eba2d1ce37c7892308ad64e5b0e94ce02a40
braianb92 <47337939+braianb92@users.noreply.github.com>
2019-02-06 21:33:27 -0300

    Add files via upload

--- 2-InstruccionIf/jsInstruccionIF (6).js
+++ 2-InstruccionIf/jsInstruccionIF (6).js
@@ -1,7 +1,24 @@
 function Mostrar()
 {
 //tomo la edad  
+var edad;
 
+edad = document.getElementById("edad").value;
+
+if (edad >= 18)
+{
+    alert("mayor de edad");
+}
+
+if (edad >= 13 & edad <= 17)
+{
+    alert("adolescente");
+}
+
+if (edad < 13)
+{
+    alert("niño");
+}
 
 
 
