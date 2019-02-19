2c2f4d586f7ee4b7b83e7a316c84a0bed446310a
braianb92 <47337939+braianb92@users.noreply.github.com>
2019-02-18 22:16:02 -0300

    Add files via upload

---
+++ jsInstruccionSwitch(2).js
@@ -0,0 +1,35 @@
+function Mostrar()
+{
+//tomo el mes 
+var mesDelAño = document.getElementById('mes').value;
+
+
+
+switch (mesDelAño)
+{
+    case "Julio":
+    
+    case "Agosto":
+    alert("Abrigate que hace frio");
+    break;
+
+    case "Agosto":
+
+    case "Septiembre":
+
+    case "Octubre":
+
+    case "Noviembre":
+
+    case "Diciembre":
+    alert("Ya pasamos el frio, ahora calor!!");
+    break;
+
+    default:
+    alert("Falta para el invierno");
+
+}
+
+
+
+}//FIN DE LA FUNCIÓN
---
+++ jsInstruccionSwitch(4).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+//tomo el mes 
+var mesDelAño = document.getElementById('mes').value;
+
+//alert (mesDelAño);
+    switch (mesDelAño)
+    {
+        case "Febrero":
+        alert("tiene 28 días");
+        break;
+
+        case "Abril":
+        case "Junio":
+        case "Septiem"
+    }
+	
+
+
+
+}//FIN DE LA FUNCIÓN
