2e1e833ea2701afb0cc095655aa1ad1fc5a16ec2
braianb92 <47337939+braianb92@users.noreply.github.com>
2019-02-20 15:36:30 -0300

    Add files via upload

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -4,7 +4,23 @@ function Mostrar()
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
+    switch (mesDelAño)
+    {
+        case "Febrero":
+        alert("tiene 28 días");
+        break;
+
+        case "Abril":
+        case "Junio":
+        case "Septiembre":
+        case "Noviembre":
+        alert("tiene 30 dias");
+        break;
+
+        default:
+        alert("tiene 31 dias");
+        break;
+    }
 	
 
 
