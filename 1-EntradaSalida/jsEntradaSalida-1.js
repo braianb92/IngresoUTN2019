9c862bb403d33a1d37ae8f3981db25d3949f8828
braianb92 <47337939+braianb92@users.noreply.github.com>
2019-02-27 21:42:51 -0300

    Add files via upload

--- 1-EntradaSalida/jsEntradaSalida-1.js
+++ 1-EntradaSalida/jsEntradaSalida-1.js
@@ -1,6 +1,58 @@
-//Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
-function Mostrar()
-{
-	alert("Esto funciona de maravilla")
+/*
+Realizar el algoritmo que permita el ingreso por prompt de las notas (validar entre 0 y 10) y el sexo 
+(validar el sexo “f”  o “m”)  de 100 alumnos, informar por alert:
+a)
+el promedio de las notas totales.
+b)
+la nota más baja.
+c)
+la cantidad de varones que su nota haya sido mayor o igual a  6 */
+function Mostrar() {
+	var nota;
+	var sexo;
+	var promedio;
+	var notabaja;
+	var contadorvarones = 0;
+	var acumulador = 0;
+	var flag = 0;
+	var sexonotabaja;
+	var cantidad = 3;
+
+	for (var i = 0; i < cantidad; i++) {
+		nota = parseInt(prompt("Ingrese nota"));
+
+		while (nota < 0 || nota > 10 || isNaN(nota)) {
+			nota = parseInt(prompt("Error. Ingrese nota"));
+		}
+
+		sexo = prompt("Ingrese sexo f/m").toLowerCase();
+
+		while (sexo != "f" && sexo != "m") {
+			sexo = prompt("Error. Ingrese sexo f/m").toLowerCase();
+		}
+
+		acumulador = acumulador + nota;
+
+		if (sexo == "m" && nota >= 6) {
+			contadorvarones++;
+		}
+
+		if (nota < notabaja || flag == 0) {
+			notabaja = nota;
+			sexonotabaja = sexo;
+			flag = 1;
+		}
+
+
+
+	}
+
+
+	promedio = acumulador / cantidad;
+
+	alert("Promedio de las notas: " + promedio + "\nNota mas baja: " +
+	 notabaja + "\nVarones con nota mayor o igual a 6: " + contadorvarones +
+	  "\nSexo nota mas baja: " + sexonotabaja); //\n es lo mismo que el <br>
+
 }
 
--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,11 +1,71 @@
-/*Debemos lograr tomar un nombre con 'prompt' 
-y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+/*realizar el algoritmo que permita ingresar números positivos (validar que sea positivo) hasta que el 
+usuario quiera e informar al terminar el ingreso por document.write: 
+a)
+la cantidad de números pares.
+b)
+el promedio de todos los números ingresados.
+c)
+la suma de todos los números.
+d)
+El número máximo y el mínimo.*/
 function Mostrar() 
 {
-    var nombre
+ //variables
+ var num;
+ var respuesta;
+ var contadorpar = 0;
+ var acumulador = 0;
+ var maximo;
+ var minimo;
+ var promedio;
+ var contador =0;
+ var flag = 0;
 
-    nombre = prompt("Ingrese su nombre");
+//iteracion
+do
+{
+ num = parseInt(prompt("Ingrese un numero positivo"));
 
-    alert(nombre)
+//validacion dentro de iteracion
+ while(num < 0)
+ {
+     num = parseInt(prompt("Error. Ingrese un numero positivo"));
+ }
+
+//sumo todos los numeros
+ acumulador = acumulador + num;
+
+//determino si son pares
+if(num % 2 == 0)
+{
+    contadorpar++;
 }
-   
+
+//maximos y minimos
+if(num > maximo || flag == 0)
+{
+    maximo = num;
+}
+
+if(num < minimo || flag == 0)
+{
+    minimo = num;
+    flag++;
+}
+
+
+
+contador++;
+
+//seguir o cancelar
+ respuesta = confirm("¿Desea ingresar otro numero?");
+
+}while(respuesta);
+//fin de iteracion
+
+//promedio fuera de iteracion
+promedio = acumulador / contador;
+
+alert("La cantidad de numeros pares: " + contadorpar + "\nEl promedio de todos los numeros ingresados: " + promedio + "\nLa suma de todos los numeros: " + acumulador +"\nNumero maximo: " + maximo + "\nNumero minimo: " + minimo);
+
+}
