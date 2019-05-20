afb7443cefef848cffaeafc61f5155131bedb0a0
Baldino Braian <baldino.bab@gmail.com>
2019-05-20 17:38:22 -0300

    Add files via upload

---
+++ Baldino/Clase1-C#/Hola/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hola
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Console.WriteLine("Holaaa");
+            Console.ReadKey();
+            string nombre;
+            const int DiasSemana = 7;
+            const int HorasPorDia = 24;
+            Console.Write("Ingrese nombre:");
+            nombre = Console.ReadLine();
+            Console.WriteLine("El nombre ingresado es {0}", nombre);
+            Console.WriteLine("El nombre ingresado es " + nombre);
+            Console.WriteLine("La cantidad de horas de una semana es {0}",
+                              DiasSemana * HorasPorDia);*/
+            /*int a = 2;
+            int b = 3;
+            int c = a + b;
+            Console.WriteLine("Variable a tiene {0}", a);
+            Console.WriteLine("Variable b tiene {0}", b);
+            Console.WriteLine("Variable c tiene {0}", c);
+            Console.WriteLine("La suma es {0}", a+b+c);*/
+
+            /*string p;
+            string l;
+            string f;
+
+            p = "Perro";
+            l = "ladra";
+            f = "fuerte";
+
+            Console.WriteLine(p + l);
+            Console.WriteLine(p + " " + l);
+            Console.WriteLine("El {0} que {1} muy {2}", p, l, f);*/
+
+            /*char char1 = 'Z'; 
+             * 
+            char char2 = (char)88; //Valor tabla ASCII convertido a Char
+            Console.WriteLine(char1 + " " + char2);*/
+
+            /*bool z;
+            z = true;
+            Console.WriteLine(z);
+            z = false;
+            Console.WriteLine(z);*/
+
+            float fl = 0;
+            Console.WriteLine(fl);
+            fl = 95.60f;
+            Console.WriteLine(fl);
+
+            double dl = 0;
+            Console.WriteLine(dl);
+            dl = 95.60d;
+            Console.WriteLine(dl);
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Baldino/Clase1-C#/Hola/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Hola")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Hola")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("bad4c674-ccf9-40a1-b7d3-fd73abcd2904")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
